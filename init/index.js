210556da6f4c0a5f570d2ef4ee71286264181e4d
nitintiwari27 <nitintiwari5111@gmail.com>
2025-08-04 22:57:37 +0530

    complete

--- init/index.js
+++ init/index.js
@@ -4,7 +4,7 @@ const Listing = require("../models/listing.js");
 const User = require("../models/user.js"); // Add this
 const axios = require("axios");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/Findhouse";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
