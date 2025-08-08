ae9d79097d91f16749576d4aecd9dab08a5617c0
nitintiwari27 <nitintiwari5111@gmail.com>
2025-08-08 14:16:07 +0530

    footer changes

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", async (req, res) => {
-  const listings = await Listing.find().limit(8);
+  const listings = await Listing.find().limit(3);
   res.render("listings/homepage", { listings });
 });
 
